cf5db96f6435c99d0615ede225e1fc53e4d76130
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-07-12 15:22:17 +0530

    Create Tiling_Mcross1.java

---
+++ Tiling_Mcross1.java
@@ -0,0 +1,30 @@
+/*
+https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/tiling2-official/ojquestion
+*/
+
+
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+   Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[] dp = new int[n + 1];
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      if (i < m) {
+        dp[i] = 1;
+      } else if (i == m) {
+        dp[i] = 2;
+      } else {
+        dp[i] = dp[i - 1] + dp[i - m];
+      }
+    }
+
+    System.out.println(dp[n]);
+  }
+}
