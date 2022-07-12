aa9338c436558803dc74518e82ba40adddcec2e9
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-07-12 15:37:11 +0530

    Create friends_pair.java

---
+++ friends_pair.java
@@ -0,0 +1,31 @@
+/*
+https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/friends-pairing-official/ojquestion
+*/
+
+
+
+
+
+
+
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] dp = new int[n + 1];
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + (i - 1) * dp[i - 2];
+    }
+    System.out.println(dp[n]);
+    scn.close();
+  }
+}
