fc8b71f6a8c8319439d350135e231169c0222df4
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-07-14 13:52:01 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -155,3 +155,149 @@ public class Main {
     }*/
 
 }
+
+/*
+check loop in directed graph*/
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for(int i = 0; i < V+1; i++)
+                list.add(i, new ArrayList<Integer>());
+                
+            for(int i = 0; i < E; i++)
+            {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if(isCyclic(V, list) == true)
+                System.out.println("Yes");
+            else System.out.println("No");
+    }
+    public static boolean isCyclic(int V, ArrayList<ArrayList<Integer>> list ){
+        boolean visited[]=new boolean[V];
+        boolean recS[]=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(dfsCheck(i, list, visited, recS))
+                 return true;
+            }
+        }
+        return false;
+
+    }
+    public static boolean dfsCheck(int start, ArrayList<ArrayList<Integer>> list, boolean[] visited, boolean[] recS){
+        visited[start]=true;
+        recS[start]=true;
+
+        for(Integer neighbour: list.get(start)){
+            if(!visited[neighbour]){
+                if(dfsCheck(neighbour, list, visited, recS))
+                  return true;
+            }
+            else if(recS[neighbour]){
+                return true;
+            }
+        }
+        recS[start]=false;
+        return false;
+    }
+
+
+
+}
+/*class Solution {
+    private boolean checkCycle(int node,  ArrayList<ArrayList<Integer>> adj, int vis[], int dfsVis[]) {
+        vis[node] = 1; 
+        dfsVis[node] = 1; 
+        
+        for(Integer it: adj.get(node)) {
+            if(vis[it] == 0) {
+                if(checkCycle(it, adj, vis, dfsVis) == true) {
+                    return true; 
+                }
+            } else if(dfsVis[it] == 1) {
+                return true; 
+            }
+        }
+        dfsVis[node] = 0;
+        return false; 
+    }
+    public boolean isCyclic(int N, ArrayList<ArrayList<Integer>> adj) {
+        int vis[] = new int[N];     
+        int dfsVis[] = new int[N]; 
+        for(int i = 0;i<N;i++) {
+            if(vis[i] == 0) {
+                if(checkCycle(i, adj, vis, dfsVis) == true) return true; 
+            }
+        }
+        return false; 
+    }*/
+/*
+check looop in undirectedf graph
+*/
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+            ArrayList<TreeSet<Integer>> list = new ArrayList<>();
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            for(int i = 0; i < N+1; i++)
+                list.add(i, new TreeSet<Integer>());
+                
+            for(int i = 0; i < M; i++)
+            {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+                list.get(v).add(u);
+            }
+            if(isCyclic(N, list) == true)
+                System.out.println("Yes");
+            else System.out.println("No");
+    }
+    public static boolean isCyclic(int N, ArrayList<TreeSet<Integer>> list ){
+        boolean visited[]=new boolean[N];
+        
+        for(int i=0;i<N;i++){
+            if(!visited[i]){
+                if(dfsCheck(i, list, visited, -1))
+                 return true;
+            }
+        }
+        return false;
+
+    }
+    public static boolean dfsCheck(int cur, ArrayList<TreeSet<Integer>> list, boolean[] visited, int parent){
+        visited[cur]=true;
+       
+
+        for(Integer neighbour: list.get(cur)){
+            if(!visited[neighbour]){
+                if(dfsCheck(neighbour, list, visited,cur))
+                  return true;
+            }
+            else if(parent!=neighbour){
+                return true;
+            }
+        }
+      
+        return false;
+    }
+
+
+
+}
