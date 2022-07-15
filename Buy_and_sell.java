b0cb0655874312de184cfe57049082bfa7ff5087
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-07-15 13:53:01 +0530

    Update Buy_and_sell.java

--- Buy_and_sell.java
+++ Buy_and_sell.java
@@ -258,3 +258,51 @@ public class Main {
     }
 
 }
+
+/*
+https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/buy-and-sell-stocks-kta-official/ojquestion
+
+ktransactions at most
+
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    int k=sc.nextInt();
+    
+    int dp[][]=new int[k+1][n];
+    
+    for(int t=1;t<dp.length;t++){
+        
+        int max=Integer.MIN_VALUE;
+        for(int d=1;d<dp[0].length;d++){
+            if(dp[t-1][d-1]-arr[d-1] > max){
+                max = dp[t-1][d-1]-arr[d-1];
+            }
+            
+            if(max + arr[d] > dp[t][d-1]){
+                dp[t][d] = max + arr[d];
+            }else{
+                dp[t][d] = dp[t][d-1];
+            }
+        }
+   
+    }
+    
+    System.out.println(dp[k][n-1]);
+  }
+
+}
