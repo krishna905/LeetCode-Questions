a55a9bc9fae2f6ffa0df42c096b13e7b9c8f6b3e
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-07-17 10:13:09 +0530

    Update printAllMinPaths.java

--- printAllMinPaths.java
+++ printAllMinPaths.java
@@ -83,3 +83,94 @@ public class Main{
        
     }
 }
+
+
+/*
+https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/dynamic-programming/minimum-cost-path-re-official/ojquestion
+
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  private static class Pair {
+    String psf;
+    int i;
+    int j;
+
+    public Pair(String psf, int i, int j) {
+      this.psf = psf;
+      this.i = i;
+      this.j = j;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+   Scanner sc=new Scanner(System.in);
+    int n = sc.nextInt();
+    int m =  sc.nextInt();
+    int[][] arr = new int[n][m];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        arr[i][j] =  sc.nextInt();
+      }
+    }
+
+    int [][]dp = new int[arr.length][arr[0].length];
+        
+       
+        for(int i=dp.length-1; i>=0 ; i--){
+            for(int j=dp[0].length-1 ; j>=0 ; j--){
+                
+                if(i==dp.length-1 && j==dp[0].length-1){
+                        dp[i][j]=arr[i][j];
+                }else if(i==dp.length-1){
+                    dp[i][j]=arr[i][j]+dp[i][j+1];
+                }else if(j==dp[0].length-1){
+                    dp[i][j]=arr[i][j]+dp[i+1][j];         
+                }else{
+                    dp[i][j]=arr[i][j]+Math.min(dp[i][j+1], dp[i+1][j]);
+                    
+                }
+            }
+        }
+        System.out.println(dp[0][0]);
+        ArrayDeque<Pair> q=new ArrayDeque<>();
+        q.add(new Pair("", 0, 0));
+        
+        while(!q.isEmpty()){
+            Pair rem= q.pollFirst();
+            
+            if(rem.i ==  dp.length-1 && rem.j == dp[0].length-1){
+                System.out.println(rem.psf);
+                
+            }else if(rem.i==dp.length-1){
+                q.add(new Pair(rem.psf+"H", rem.i,rem.j+1));
+                
+            }else if(rem.j==dp[0].length-1){
+                q.add(new Pair(rem.psf+"V", rem.i+1,rem.j));
+                
+            }else{
+                if(dp[rem.i+1][rem.j] < dp[rem.i][rem.j+1]){
+                    q.add(new Pair(rem.psf+"V", rem.i+1,rem.j));
+                    
+                }else if(dp[rem.i+1][rem.j] > dp[rem.i][rem.j+1]){
+                    q.add(new Pair(rem.psf+"H", rem.i,rem.j+1));
+                    
+                }else{
+                     q.add(new Pair(rem.psf+"V", rem.i+1,rem.j));
+                     q.add(new Pair(rem.psf+"H", rem.i,rem.j+1));
+                    
+                }
+                
+            }
+            
+        }
+
+  }
+
+}
