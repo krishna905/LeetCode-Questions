d2ec3d77307ea93a43318fe555bb487fd336e245
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-07-19 01:19:45 +0530

    Create 43.catalan_num.java

---
+++ 43.catalan_num.java
@@ -0,0 +1,32 @@
+/*
+https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/dynamic-programming/catalan-number-official/ojquestion
+*/
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int dp[]=new int[n+1];
+    
+      
+      dp[0]=1; 
+      dp[1]=1;
+      
+      for(int i=2;i<=n;i++)
+      {
+          for(int j=0;j<i;j++)
+          {
+              dp[i]+=dp[j]*dp[i-j-1];
+          }
+      }
+        System.out.println(dp[n]);
+ }
+
+}
