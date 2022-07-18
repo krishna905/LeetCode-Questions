440fa4469d982fda99112ab5eeb37e5c1deb9420
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-07-18 23:14:13 +0530

    Create print_all_longest_inc_seq.java

---
+++ print_all_longest_inc_seq.java
@@ -0,0 +1,92 @@
+/*
+https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/dynamic-programming/lis-re-official/ojquestion
+*/
+
+
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main{
+    public static class Pair{
+        int idx;
+        int len;
+        int val;
+        String psf;
+        Pair(int idx, int len, int val, String psf){
+            this.idx=idx;
+            this.len=len;
+            this.val=val;
+            this.psf=psf;
+        }
+    }
+    
+        
+    public static void solution(int []arr){
+       
+        
+        int omax = 0;
+        int omaxidx = 0;
+        int[]dp = new int[arr.length];
+        
+            for(int i = 0; i < dp.length; i++) {
+                int max = 0;
+                for(int j = 0; j < i; j++){
+                  
+                  if (arr[j] <= arr[i]){
+                     if (dp[j] > max){
+                     max = dp[j];
+                     }
+                    }
+                }
+              
+                 dp[i] = max + 1;
+                 if(dp[i] > omax) {
+                    omax = dp[i];
+                    omaxidx=i;
+                }
+            }
+       System.out.println(omax);
+       
+       ArrayDeque<Pair> q=new ArrayDeque<>();
+       
+       q.add(new Pair(omaxidx,omax, arr[omaxidx], arr[omaxidx]+""));
+       
+       while(!q.isEmpty()){
+           Pair rem= q.pollFirst();
+           
+           if(rem.len == 1){
+               System.out.println(rem.psf);
+           }
+           
+           for(int j=rem.idx-1;j>=0;j--){
+               if(dp[j]==rem.len-1 && arr[j]<=rem.val){
+                    q.add(new Pair(j,dp[j], arr[j], arr[j]+" -> "+rem.psf));
+                   
+                   
+               }
+               
+           }
+       }
+       
+    }
+    
+    
+    
+    public static void main(String []args){
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int arr[] = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = scn.nextInt();
+        }
+
+        solution(arr);
+
+       
+    }
+}
