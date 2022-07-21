15c3a0870ca51383775e1f7f6558a402db303bbc
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-07-22 00:13:15 +0530

    Update coinChange_combinations.java

--- coinChange_combinations.java
+++ coinChange_combinations.java
@@ -27,3 +27,26 @@ class Solution {
         return dp[amount];
     }
 }
+
+
+/*
+
+----correct code -------
+int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+         dp[0]=0;
+        
+        for(int i=1;i<dp.length;i++){
+            
+            for(int j=0; j<coins.length;j++){
+                
+                if(coins[j]<=i)
+                dp[i] =Math.min(dp[i], 1+dp[i-coins[j]]) ;
+            }
+        }
+        int res= dp[amount]!=amount+1? dp[amount] : -1;
+       System.out.println(res);
+
+
+
+*/
