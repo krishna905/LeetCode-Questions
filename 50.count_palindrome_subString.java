7c174145e6cccd9756ca3e172e5a33a2c7527f02
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-07-22 00:52:43 +0530

    Create 50.count_palindrome_subString.java

---
+++ 50.count_palindrome_subString.java
@@ -0,0 +1,43 @@
+/*
+https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/dynamic-programming/cpss-official/ojquestion
+*/
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+    Scanner sc= new Scanner(System.in);
+    String s= sc.nextLine();
+    int count=0;
+    
+    boolean dp[][]=new boolean[s.length()][s.length()];
+    
+    for(int g=0;g<s.length();g++){
+        for(int i=0, j=g; j<dp.length; i++,j++){
+            
+            if(g == 0){
+                dp[i][j] = true;
+             } else if(g == 1){
+                 dp[i][j] = s.charAt(i) == s.charAt(j);
+             } else{
+                 if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true){
+                     dp[i][j] = true;
+                 } else{
+                     dp[i][j]= false;
+                 }
+             }
+             if(dp[i][j]== true){
+                 count++;
+             }
+        
+        }
+     }
+     System.out.println(count);
+
+    }
+
+}
